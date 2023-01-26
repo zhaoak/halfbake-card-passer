1060a33a4f9dcdb4684b54c805f6fae302f86f9b
Allie Zhao <allie.k.zhao@gmail.com>
2023-01-26 12:07:54 -0800

    Player and Card components now use context instead of being passed info thru props

--- src/App.js
+++ src/App.js
@@ -63,34 +63,10 @@ function App() {
     <div className="App">
       <section>
         {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
-        <Player
-          player={1}
-          hand={playerOneHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setSelectedCard={setSelectedCard}
-        />
-        <Player
-          player={2}
-          hand={playerTwoHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setSelectedCard={setSelectedCard}
-        />
-        <Player
-          player={3}
-          hand={playerThreeHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setSelectedCard={setSelectedCard}
-        />
-        <CardList
-          cards={deck}
-          selectedCard={selectedCard}
-          setSelectedCard={setSelectedCard}
-          setFrom={setFrom}
-          player={'deck'}
-        />
+        <Player player={1} hand={playerOneHand} />
+        <Player player={2} hand={playerTwoHand} />
+        <Player player={3} hand={playerThreeHand} />
+        <CardList cards={deck} player={'deck'} />
       </section>
       <section>
         {selectedCard && (
--- src/components/Player.js
+++ src/components/Player.js
@@ -3,9 +3,9 @@ import CardList from './CardList';
 import { useContext } from 'react';
 import { useGameContext } from '../context/GameContext.js';
 
-export default function Player({ player, hand, setFrom, setSelectedCard, selectedCard }) {
-  // only taking these two from context because other state vars need to be per player (and thus passed thru props), not shared among all player components
-  const { to, setTo } = useGameContext();
+export default function Player({ player, hand }) {
+  // player and hand are per-player and thus must be passsed as props to individual Player components
+  const { to, setTo, setSelectedCard, selectedCard, setFrom } = useGameContext();
   return (
     <div
       className={`player ${to === player ? 'selected-player' : ''}`}
